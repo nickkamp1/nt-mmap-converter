811c10015c070814a88c4b2fd143e0980cfc5262
Nicholas Kamp <nkamp@fas.harvard.edu>
2026-03-24 14:18:29 -0400

    BDT score cuts in conversion scripts

--- Mag1_jobs/convert_all.sh
+++ Mag1_jobs/convert_all.sh
@@ -1,15 +1,15 @@
 
 # background
-sbatch --export TAG=hnl_tracks/background/22803_extended,FILETYPE=CORSIKA convert.sbatch
-sbatch --export TAG=hnl_tracks/background/22852,FILETYPE=NuGen convert.sbatch
-sbatch --export TAG=hnl_tracks/background/22853,FILETYPE=NuGen convert.sbatch
-sbatch --export TAG=hnl_tracks/background/22855,FILETYPE=NuGen convert.sbatch
-sbatch --export TAG=hnl_tracks/background/22856,FILETYPE=NuGen convert.sbatch
-sbatch --export TAG=hnl_tracks/background/23257,FILETYPE=NuGen convert.sbatch
-sbatch --export TAG=hnl_tracks/background/23258,FILETYPE=NuGen convert.sbatch
+sbatch --export TAG=hnl_tracks_with_bdt/background/22803_extended,FILETYPE=CORSIKA convert.sbatch
+sbatch --export TAG=hnl_tracks_with_bdt/background/22852,FILETYPE=NuGen convert.sbatch
+sbatch --export TAG=hnl_tracks_with_bdt/background/22853,FILETYPE=NuGen convert.sbatch
+sbatch --export TAG=hnl_tracks_with_bdt/background/22855,FILETYPE=NuGen convert.sbatch
+sbatch --export TAG=hnl_tracks_with_bdt/background/22856,FILETYPE=NuGen convert.sbatch
+sbatch --export TAG=hnl_tracks_with_bdt/background/23257,FILETYPE=NuGen convert.sbatch
+sbatch --export TAG=hnl_tracks_with_bdt/background/23258,FILETYPE=NuGen convert.sbatch
 # signal
-# sbatch --export TAG=hnl_tracks/signal/DipoleDIS,FILETYPE=SIREN convert.sbatch
-sbatch --export TAG=hnl_tracks/signal/MixingDIS/nue,FILETYPE=SIREN convert.sbatch
-sbatch --export TAG=hnl_tracks/signal/MixingDIS/numu,FILETYPE=SIREN convert.sbatch
-sbatch --export TAG=hnl_tracks/signal/MixingDIS/nuebar,FILETYPE=SIREN convert.sbatch
-sbatch --export TAG=hnl_tracks/signal/MixingDIS/numubar,FILETYPE=SIREN convert.sbatch
+#sbatch --export TAG=hnl_tracks_with_bdt/signal/DipoleDIS,FILETYPE=SIREN convert.sbatch
+sbatch --export TAG=hnl_tracks_with_bdt/signal/MixingDIS/nue,FILETYPE=SIREN convert.sbatch
+sbatch --export TAG=hnl_tracks_with_bdt/signal/MixingDIS/numu,FILETYPE=SIREN convert.sbatch
+sbatch --export TAG=hnl_tracks_with_bdt/signal/MixingDIS/nuebar,FILETYPE=SIREN convert.sbatch
+sbatch --export TAG=hnl_tracks_with_bdt/signal/MixingDIS/numubar,FILETYPE=SIREN convert.sbatch
